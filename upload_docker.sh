1fb5ce293857cf618013f363fa90c36e391f5911
Travis Smith <travis.a.smith.ja@gmail.com>
2020-04-15 08:41:42 -0500

    ops: Adds script to upload to Dockerhub.

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,17 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=travissmith94/sklearn-prediction:latest
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+echo -n "Enter the docker hub password for travissmith94: "
+read -s password
+echo $password | docker login --username travissmith94 --password-stdin
+docker tag sklearn-prediction:latest $dockerpath
+docker image ls | grep travissmith94/sklearn
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
