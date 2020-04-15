e7657ff257d16ef3f82e454871ef636f621a1a89
Travis Smith <travis.a.smith.ja@gmail.com>
2020-04-15 18:30:27 -0500

    ops: Adds script to run container on kubernetes.

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=travissmith94/sklearn-prediction
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run sklearn-prediction --image=$dockerpath --port=8000
 
 # Step 3:
 # List kubernetes pods
+kubectl rollout status deployment sklearn-prediction
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward deployment/sklearn-prediction 8000:80
 
