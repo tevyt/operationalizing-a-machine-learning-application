7969e48d30e9276c05f4fd7f8a3515558f3adcea
Travis Smith <travis.a.smith.ja@gmail.com>
2020-04-15 07:29:05 -0500

    ops: Adds script file for running application.

---
+++ run_docker.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+
+DOCKER_IMAGE_TAG=sklearn-prediction
+# Step 1:
+# Build image and add a descriptive tag
+docker build -t $DOCKER_IMAGE_TAG .
+
+# Step 2: 
+# List docker images
+docker image ls
+
+# Step 3: 
+# Run flask app
+docker run -p 80:80 $DOCKER_IMAGE_TAG
